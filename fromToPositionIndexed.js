2b1ede6708a7d75418327c7fee7755618a1d2f0c
Jeff Lowery <jlowery2663@gmail.com>
2026-04-02 09:47:42 -0700

    fixes stale repo references

--- fromToPositionIndexed.js
+++ fromToPositionIndexed.js
@@ -1,24 +1,29 @@
-import fs from 'fs'
-const pos = (fen) => fen.split(' ')[0];
+import fs from "fs";
+const pos = (fen) => fen.split(" ")[0];
 
-async function fromTo () {
-    const response = await fetch('https://raw.githubusercontent.com/JeffML/eco.json/master/fromTo.json')
+async function fromTo() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/JeffML/eco.json/master/fromTo.json",
+  );
 
-    const FT = await response.json()
+  const FT = await response.json();
 
-    const fromTo = FT.reduce((acc, [from, to]) => {
-        acc.to[pos(from)] ??= []  
-        acc.to[pos(from)].push(to) // continuations from FEN
-        acc.from[pos(to)] ??= []
-        acc.from[pos(to)].push(from) // roots of FEN
-        return acc;
-    }, {to: {}, from:{}})
+  const fromTo = FT.reduce(
+    (acc, [from, to]) => {
+      acc.to[pos(from)] ??= [];
+      acc.to[pos(from)].push(to); // continuations from FEN
+      acc.from[pos(to)] ??= [];
+      acc.from[pos(to)].push(from); // roots of FEN
+      return acc;
+    },
+    { to: {}, from: {} },
+  );
 
-    return fromTo
+  return fromTo;
 }
 
 const json = await fromTo();
 
-fs.writeFileSync('./output/fromToPositionIndexed.json', JSON.stringify(json))
+fs.writeFileSync("./output/fromToPositionIndexed.json", JSON.stringify(json));
 
-console.log('done')
+console.log("done");
--- utils.js
+++ utils.js
@@ -1,123 +1,122 @@
-import { Octokit } from 'octokit';
-import readline from 'node:readline';
-import fs from 'fs';
-import path from 'path';
+import { Octokit } from "octokit";
+import readline from "node:readline";
+import fs from "fs";
+import path from "path";
 
 // not used, but kept in case github d/l urls ever change in the far future
 async function getDownloadUrls() {
-    const octokit = new Octokit(); // No auth
+  const octokit = new Octokit(); // No auth
 
-    const { data } = await octokit.request(
-        'GET /repos/{owner}/{repo}/contents/',
-        {
-            owner: 'JeffML',
-            repo: 'eco.json',
-        }
-    );
+  const { data } = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/",
+    {
+      owner: "JeffML",
+      repo: "eco.json",
+    },
+  );
 
-    const downloads = data.map((meta) => {
-        if (filesOfInterest.includes(meta.name)) return meta.download_url;
-    });
-    return downloads;
+  const downloads = data.map((meta) => {
+    if (filesOfInterest.includes(meta.name)) return meta.download_url;
+  });
+  return downloads;
 }
 
 const prompt = async (q) => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    const answer = await new Promise((resolve) => {
-        rl.question(q, resolve);
-    });
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const answer = await new Promise((resolve) => {
+    rl.question(q, resolve);
+  });
 
-    rl.close();
-    return answer;
+  rl.close();
+  return answer;
 };
 const keyLen = (o) => Object.keys(o).length;
 
 const chunker = (array, chunkSize) => {
-    const chunks = [];
+  const chunks = [];
 
-    for (let i = 0; i < array.length; i += chunkSize) {
-        chunks.push(array.slice(i, i + chunkSize));
-    }
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
 
-    return chunks;
+  return chunks;
 };
 
 function hardAssert(condition, message) {
-    console.assert(condition, message);
-    if (!condition) {
-        throw new Error(message || 'Assertion failed');
-    }
+  console.assert(condition, message);
+  if (!condition) {
+    throw new Error(message || "Assertion failed");
+  }
 }
 
 const readJsonFile = (fileName, log = false) => {
-    const FOLDER = '.';
-    const __dirname = new URL('.', import.meta.url).pathname;
+  const FOLDER = ".";
+  const __dirname = new URL(".", import.meta.url).pathname;
 
-    const file = path.join(__dirname, FOLDER, fileName);
-    const strbuf = fs.readFileSync(file);
+  const file = path.join(__dirname, FOLDER, fileName);
+  const strbuf = fs.readFileSync(file);
 
-    const openings = JSON.parse(strbuf);
+  const openings = JSON.parse(strbuf);
 
-    if (log) console.log('Read in', Object.keys(openings).length, 'records');
-    return openings;
+  if (log) console.log("Read in", Object.keys(openings).length, "records");
+  return openings;
 };
 
 let openingsByCat;
 
 // pulls the opening data from eco.json github repo
 export async function getLatestEcoJson() {
-    const ROOT =
-        'https://raw.githubusercontent.com/JeffML/eco.json/master/';
-    if (openingsByCat) return openingsByCat;
+  const ROOT = "https://raw.githubusercontent.com/JeffML/eco.json/master/";
+  if (openingsByCat) return openingsByCat;
 
-    openingsByCat = {
-        A: { url: ROOT + 'ecoA.json' },
-        B: { url: ROOT + 'ecoB.json' },
-        C: { url: ROOT + 'ecoC.json' },
-        D: { url: ROOT + 'ecoD.json' },
-        E: { url: ROOT + 'ecoE.json' },
-        IN: { url: ROOT + 'eco_interpolated.json' },
-        FT: { url: ROOT + 'fromTo.json' },
-    };
+  openingsByCat = {
+    A: { url: ROOT + "ecoA.json" },
+    B: { url: ROOT + "ecoB.json" },
+    C: { url: ROOT + "ecoC.json" },
+    D: { url: ROOT + "ecoD.json" },
+    E: { url: ROOT + "ecoE.json" },
+    IN: { url: ROOT + "eco_interpolated.json" },
+    FT: { url: ROOT + "fromTo.json" },
+  };
 
-    const promises = [];
-    for (const cat in openingsByCat) {
-        promises.push(fetch(openingsByCat[cat].url));
-    }
+  const promises = [];
+  for (const cat in openingsByCat) {
+    promises.push(fetch(openingsByCat[cat].url));
+  }
 
-    const res = await Promise.all(promises);
-    let i = 0;
+  const res = await Promise.all(promises);
+  let i = 0;
 
-    for (const cat in openingsByCat) {
-        const json = await res[i++].json();
-        openingsByCat[cat].json = json;
-    }
+  for (const cat in openingsByCat) {
+    const json = await res[i++].json();
+    openingsByCat[cat].json = json;
+  }
 
-    return openingsByCat;
+  return openingsByCat;
 }
 
 export { getDownloadUrls, keyLen, prompt, chunker, hardAssert, readJsonFile };
 
 export function convertMilliseconds(milliseconds) {
-    const totalSeconds = Math.floor(milliseconds / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
 
-    return { hours, minutes, seconds };
+  return { hours, minutes, seconds };
 }
 
 const byCat = await getLatestEcoJson();
 const { A, B, C, D, E, IN } = byCat;
 
 export const book = {
-    ...A.json,
-    ...B.json,
-    ...C.json,
-    ...D.json,
-    ...E.json,
-    ...IN.json,
+  ...A.json,
+  ...B.json,
+  ...C.json,
+  ...D.json,
+  ...E.json,
+  ...IN.json,
 };
